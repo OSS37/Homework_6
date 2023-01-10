48bef666ee71802dd38857e1034a09d24c53fa3f
OSS37 <suhanova375@gmail.com>
2023-01-10 17:12:59 +0300

    Homework_6 Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        for (int i=0; i<=10; i++) {
+            System.out.println("Итерация: " + i);
+        }
+
+        System.out.println("Задача 2");
+        for (int t=10; t>=0; t--) {
+            System.out.println("Итерация: " + t);
+        }
+
+        System.out.println("Задача 3");
+        for (int i=0; i<=17; i=i+2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 4");
+        for (int t=10; t>=-10; t--) {
+            System.out.println(t);
+        }
+
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4){
+            System.out.println(i + " год является високосным");
+        }
+
+        System.out.println("Задача 6");
+        for (int i=7; i<=98; i=i+7) {
+            System.out.println(i + ", ");
+        }
+
+        System.out.println("Задача 7");
+        for (int i=1; i<=512; i=i*2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 8");
+        int money = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + money;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + "рублей");
+        }
+
+        System.out.println("Задача 9");
+        int money1 = 29000;
+                int total1 = 0;
+        for (int i = 1; i <= 12; i++) {
+            total1 = total1+total1/100;
+            total1 = total1 + money1;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total1 + "рублей");
+        }
+
+        System.out.println("Задача 10");
+        int num = 1;
+        for (int i = 1; i <= 10; i++) {
+            num = i*2;
+            System.out.println("2*"+i+"="+num);
+        }
+
     }
 }
